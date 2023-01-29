850a280afadf08f33452b3ab92eabfddcbdcc76f
SamarthTumdi <samarth.tumdi@gmail.com>
2023-01-29 23:41:25 +0530

    Added Vector Shopping List

---
+++ VectorShoppingList.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+class VectorShoppingList {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int choice, noofitems;
+    String inputString;
+    Vector<String> shoppingList = new Vector<String>(5);
+    
+    System.out.print("Enter number of items: ");
+    noofitems = sc.nextInt(); sc.nextLine();
+    System.out.println("Enter Shopping List items: ");
+    for(int i = 0; i < noofitems; i++) {
+      inputString = sc.nextLine();
+      shoppingList.addElement(inputString);
+    }
+
+    while(true) {
+      System.out.println("\nEnter the choice for corresponding actions: ");
+      System.out.print("1. Add item to specified location.\n2. Delete an item.\n3. Print Shopping List.\nChoice: ");
+      choice = sc.nextInt(); sc.nextLine();
+  
+      switch(choice) {
+        default:
+          System.out.println("Invalid Choice!");
+          sc.close();
+          return;
+
+        case 1:
+          System.out.print("Enter new item: ");
+          inputString = sc.nextLine();
+          System.out.print("Enter position to add at: ");
+          choice = sc.nextInt();
+          shoppingList.insertElementAt(inputString, choice);
+          System.out.println("Item added!");
+        break;
+          
+        case 2:
+          System.out.print("Enter item to delete: ");
+          inputString = sc.nextLine();
+          shoppingList.removeElement(inputString);
+        break;
+
+        case 3:
+          System.out.println("==== Shopping List ====");
+          System.out.println(shoppingList);
+        break;
+      }
+    }
+  }
+}
